c8c0e8c6101da38e663ac6385224f77f4196f987
silkyprogrammer <kris.palappetty@gmail.com>
2013-02-21 22:34:01 +0530

    changed app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-http = require("http")
+var http = require("http")
+var server = http.createServer(function(req,resp){
+  resp.writeHead(200,{"Content-type":"text/plain"});
+  resp.end("Hello World");
+});
+
+server.listen(8000);
+console.log("Listening on port http://127.0.0.1:8000");
